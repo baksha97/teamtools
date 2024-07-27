3303e754ce653cc1c05e680b36559e48aa2d94ae
Alfayed Baksh <baksha97@gmail.com>
2024-07-27 16:59:24 +0000

    update redirect logic

--- src/routes/auth/login/google/+server.ts
+++ src/routes/auth/login/google/+server.ts
@@ -1,10 +1,14 @@
 import { redirect } from "@sveltejs/kit";
 
 export const GET = async ({ locals: { supabase }, url }) => {
+  let redirectedUrl =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    url.origin;
   const { data } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: url.origin + "/auth/callback",
+      redirectTo: redirectedUrl + "auth/callback",
     },
   });
 
